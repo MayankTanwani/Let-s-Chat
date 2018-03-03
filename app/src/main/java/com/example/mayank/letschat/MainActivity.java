817b6efd6393b3d5b5267f5717e7664f1ee6215f
Mayank Tanwani <tanwani.mayank@gmail.com>
2018-03-04 01:49:56 +0530

    removed round icons

--- app/src/main/java/com/example/mayank/letschat/MainActivity.java
+++ app/src/main/java/com/example/mayank/letschat/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mayank.letschat;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
